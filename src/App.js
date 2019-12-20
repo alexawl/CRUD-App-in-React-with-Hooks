42c484e52cd0ca4692d932ef970f988309d02c2e
alexawl <alexawl@bellsouth.net>
2019-12-20 20:24:57 +0000

    final

--- src/App.js
+++ src/App.js
@@ -1,32 +1,76 @@
-import React, { useState } from 'react'
+import React, { useState, Fragment } from 'react'
+import AddUserForm from './forms/AddUserForm'
+import EditUserForm from './forms/EditUserForm'
+import UserTable from './tables/UserTable'
 
-const AddUserForm = props => {
-  const initialFormState = { id: null, name: '', username: '' }
-  const [user, setUser] = useState(initialFormState)
+const App = () => {
+	// Data
+	const usersData = [
+		{ id: 1, name: 'Tania', username: 'floppydiskette' },
+		{ id: 2, name: 'Craig', username: 'siliconeidolon' },
+		{ id: 3, name: 'Ben', username: 'benisphere' },
+	]
 
-  const handleInputChange = event => {
-    const { name, value } = event.target
+	const initialFormState = { id: null, name: '', username: '' }
 
-    setUser({ ...user, [name]: value })
-  }
+	// Setting state
+	const [ users, setUsers ] = useState(usersData)
+	const [ currentUser, setCurrentUser ] = useState(initialFormState)
+	const [ editing, setEditing ] = useState(false)
 
-  return (
-    <form
-      onSubmit={event => {
-        event.preventDefault()
-        if (!user.name || !user.username) return
+	// CRUD operations
+	const addUser = user => {
+		user.id = users.length + 1
+		setUsers([ ...users, user ])
+	}
 
-        props.addUser(user)
-        setUser(initialFormState)
-      }}
-    >
-      <label>Name</label>
-      <input type="text" name="name" value={user.name} onChange={handleInputChange} />
-      <label>Username</label>
-      <input type="text" name="username" value={user.username} onChange={handleInputChange} />
-      <button>Add new user</button>
-    </form>
-  )
+	const deleteUser = id => {
+		setEditing(false)
+
+		setUsers(users.filter(user => user.id !== id))
+	}
+
+	const updateUser = (id, updatedUser) => {
+		setEditing(false)
+
+		setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+	}
+
+	const editRow = user => {
+		setEditing(true)
+
+		setCurrentUser({ id: user.id, name: user.name, username: user.username })
+	}
+
+	return (
+		<div className="container">
+			<h1>CRUD App with Hooks</h1>
+			<div className="flex-row">
+				<div className="flex-large">
+					{editing ? (
+						<Fragment>
+							<h2>Edit user</h2>
+							<EditUserForm
+								editing={editing}
+								setEditing={setEditing}
+								currentUser={currentUser}
+								updateUser={updateUser}
+							/>
+						</Fragment>
+					) : (
+						<Fragment>
+							<h2>Add user</h2>
+							<AddUserForm addUser={addUser} />
+						</Fragment>
+					)}
+				</div>
+				<div className="flex-large">
+					<h2>View users</h2>
+					<UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default AddUserForm
+export default App
--- src/tables/UserTable.js
+++ src/tables/UserTable.js
@@ -10,23 +10,35 @@ const UserTable = props => (
       </tr>
     </thead>
     <tbody>
-        {props.users.length > 0 ? (
-            props.users.map(user => (
-                   <tr key={user.id}>
-        <td>{user.name}</td>
-        <td>{user.username}</td>
-        <td>
-          <button className="button muted-button">Edit</button>
-          <button className="button muted-button">Delete</button>
-        </td>
-      </tr>   
-            ))
-        ):(
-            <tr>
-            <td colSpan={3}>No users</td>
-            </tr>
-        )}
-          </tbody>
+      {props.users.length > 0 ? (
+        props.users.map(user => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>
+              <button
+                onClick={() => {
+                  props.editRow(user)
+                }}
+                className="button muted-button"
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => props.deleteUser(user.id)}
+                className="button muted-button"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={3}>No users</td>
+        </tr>
+      )}
+    </tbody>
   </table>
 )
 
